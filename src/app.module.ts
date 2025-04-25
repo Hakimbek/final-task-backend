c3967b30f799542113e4304fbe2dde74d80e8d12
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-25 19:30:25 +0500

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+import { Module } from "@nestjs/common";
+import { DatabaseModule } from "./database/database.module";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
 import { TemplateModule } from "./template/template.module";
