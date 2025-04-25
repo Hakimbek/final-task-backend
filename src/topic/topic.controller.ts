28b7f9711985ffdcc9879453705f26d6927ad1be
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-25 19:32:54 +0500

    edit

--- src/topic/topic.controller.ts
+++ src/topic/topic.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { BadRequestException, Controller, Get } from "@nestjs/common";
 import { TopicService } from "./topic.service";
 
-@Controller('topic')
+@Controller("topic")
 export class TopicController {
     constructor(private readonly topicService: TopicService) {}
 
     @Get()
     async getAllTopics() {
-        return await this.topicService.getAllTopics();
+        try {
+            return await this.topicService.getAllTopics();
+        } catch (error) {
+            throw new BadRequestException(error.response);
+        }
     }
 }
