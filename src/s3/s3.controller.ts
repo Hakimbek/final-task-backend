f7aecf922549bff6f511875c7ed9b67a95bde758
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-25 22:38:49 +0500

    edit

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Get, Query, UseGuards, Res, HttpStatus } from '@nestjs/common';
-import { S3Service } from './s3.service';
+import { Controller, Get, Query, UseGuards, BadRequestException } from "@nestjs/common";
+import { S3Service } from "./s3.service";
 import { JwtAuthGuard } from "../jwt/jwt-auth.guard";
-import { Response } from "express";
 
-@Controller('upload')
+@Controller("signed-url")
 export class UploadController {
     constructor(private readonly s3Service: S3Service) {}
 
-    @Get('signed-url')
+    @Get()
     @UseGuards(JwtAuthGuard)
-    async getSignedUrl(@Query('fileName') fileName: string, @Query('fileType') fileType: string, @Res() res: Response) {
+    async getSignedUrl(
+        @Query("fileName") fileName: string,
+        @Query("fileType") fileType: string
+    ) {
         try {
-            const result = await this.s3Service.getSignedUrl(fileName, fileType);
-            res.status(HttpStatus.OK).send(result);
+            return await this.s3Service.getSignedUrl(fileName, fileType);
         } catch (error) {
-            res.status(HttpStatus.BAD_REQUEST).send(error);
+            throw new BadRequestException(error.response);
         }
     }
 }
--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { v4 as uuid } from 'uuid';
+import { Injectable } from "@nestjs/common";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class S3Service {
@@ -17,7 +17,7 @@ export class S3Service {
         const key = `${uuid()}-${fileName}`;
 
         const command = new PutObjectCommand({
-            Bucket: 'final-task-images',
+            Bucket: process.env.AWS_BUCKET_NAME,
             Key: key,
             ContentType: fileType,
         });
