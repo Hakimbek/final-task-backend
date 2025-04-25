aee7f026b31574b9d094040f664dd99cd1cd6eb6
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-25 19:29:35 +0500

    edit

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -1,6 +1,6 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
 import { UserService } from "../user/user.service";
 
 @Injectable()
@@ -11,19 +11,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'secretKey',
+      secretOrKey: process.env.JWT_SECRET || "secretKey",
     });
   }
 
-  /**
-   * Checks user status and existence. If user is active they can go forward. If not they are not allowed.
-   * The same scenario with user existence. If user with this id exists, we allow going forward.
-   * @param id - user id.
-   */
   async validate({ id }: { id: string }) {
     const user = await this.userService.findById(id);
 
-    if (!user.isActive) throw new UnauthorizedException('User is deactivated');
+    if (!user.isActive) throw new UnauthorizedException("User is deactivated");
 
     return id;
   }
