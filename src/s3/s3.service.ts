21ccb0f6b734831eb53b4aab9fbbe83df518cb4b
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-26 23:29:34 +0500

    edit

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -8,7 +8,7 @@ export class S3Service {
     private s3 = new S3Client({
         region: process.env.AWS_REGION,
         credentials: {
-            accessKeyId: process.env.AWS_REGION || '',
+            accessKeyId: process.env.AWS_ACCESS_KEY || '',
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
         }
     });
